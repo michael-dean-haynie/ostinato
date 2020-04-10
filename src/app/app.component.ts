2e99fb3c4594a2ad0100a13c7f64642fa497ea8b
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-04-09 23:40:39 -0400

    Added simple interval reminder example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ostinato';
+export class AppComponent implements OnInit {
+  simpleInterval = null;
+  simpleIntervalEnabled = true;
+  simpleIntervalMessage = 'Simple Interval Reminder';
+  simpleIntervalSeconds = 5;
+
+  ngOnInit() {
+    this.simpleIntervalReminder(this.simpleIntervalEnabled);
+  }
+
+  simpleIntervalReminder(enabled: boolean): void {
+    this.simpleIntervalEnabled = enabled;
+    if (this.simpleInterval != null) {
+      clearInterval(this.simpleInterval);
+    }
+    if (enabled) {
+      this.simpleInterval = setInterval(() => { console.log(this.simpleIntervalMessage); }, this.simpleIntervalSeconds * 1000);
+    }
+  }
 }
