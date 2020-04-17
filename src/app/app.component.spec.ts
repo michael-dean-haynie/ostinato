41338be1adb122597c2b5bec81f6cc3dcb7c55ec
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-04-17 12:56:49 -0400

    Rename project to ostinato-ui

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ostinato'`, () => {
+  it(`should have as title 'ostinato-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ostinato');
+    expect(app.title).toEqual('ostinato-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ostinato app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ostinato-ui app is running!');
   });
 });
