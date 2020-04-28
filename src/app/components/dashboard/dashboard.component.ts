1b9e50f74406df307313fa695bd29be980a692cb
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-04-27 20:54:03 -0400

    Move create form into dashboard component, include active and inactive reminder sections

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Reminder } from 'src/app/reminders/reminder';
+import { ReminderService } from 'src/app/services/reminder.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  newReminder: Reminder;
 
-  constructor() { }
+  constructor(private reminderService: ReminderService) { }
 
   ngOnInit(): void {
+    this.newReminder = this.reminderService.createReminder();
+  }
+
+  createReminder(): void {
+    this.reminderService.addReminder(this.newReminder);
+    this.newReminder = this.reminderService.createReminder();
+  }
+
+  deleteReminder(reminder: Reminder): void {
+    this.reminderService.removeReminder(reminder);
+  }
+
+  getActiveReminders(): Reminder[] {
+    return this.reminderService.reminders.filter(reminder => reminder.isActive());
+  }
+
+  getInactiveReminders(): Reminder[] {
+    return this.reminderService.reminders.filter(reminder => !reminder.isActive());
   }
 
 }
