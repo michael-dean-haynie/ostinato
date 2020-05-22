efda076bcf311bf3b1f54066de375c1a28d3845e
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-05-21 20:08:06 -0400

    Start reminder table on manager page

--- src/app/components/manager/manager.component.ts
+++ src/app/components/manager/manager.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Reminder } from 'src/app/reminders/reminder';
 import { ReminderService } from 'src/app/services/reminder.service';
 
 @Component({
@@ -8,30 +7,14 @@ import { ReminderService } from 'src/app/services/reminder.service';
   styleUrls: ['./manager.component.scss']
 })
 export class ManagerComponent implements OnInit {
-  newReminder: Reminder;
+
+  myDataArray = [];
+  columnsToDisplay = ['name'];
 
   constructor(private reminderService: ReminderService) { }
 
   ngOnInit(): void {
-    this.newReminder = this.reminderService.createReminder();
-  }
-
-  createReminder(): void {
-    this.reminderService.addReminder(this.newReminder);
-    this.newReminder = this.reminderService.createReminder();
-  }
-
-  deleteReminder(reminder: Reminder): void {
-    this.reminderService.removeReminder(reminder);
-  }
-
-  getReminders(): Reminder[] {
-    return this.reminderService.reminders;
-  }
-
-  // TODO: remove!
-  logit(): void {
-    console.log(this.reminderService.reminders);
+    this.myDataArray = this.reminderService.reminders;
   }
 
 }
